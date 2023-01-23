105b129e9fe69846fe0aaa58c8b21f7103658976
Rukshan Dias <rukshandias002@gmail.com>
2023-01-23 13:23:54 +0530

    search movies

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import { useState, useEffect } from 'react';
 
 function App() {
   const API_URL = "https://api.themoviedb.org/3/movie/popular?api_key=ae379cb0563dd1623d39e23df8e5d2e5";
+  const API_SEARCH = "https://api.themoviedb.org/3/search/movie?api_key=ae379cb0563dd1623d39e23df8e5d2e5&query="
   const [movies, setMovies] = useState([]);
+  const [term, setTerm] = useState('')
 
   useEffect(() => {
     fetch(API_URL)
@@ -14,6 +16,15 @@ function App() {
 
   console.log(movies)
 
+  const handleSearch = (e) => {
+    e.preventDefault()
+    console.log("printing searched data" + API_SEARCH+term)
+    fetch(API_SEARCH + term)
+      .then(res => res.json())
+      .then(data => setMovies(data.results))
+    console.log(movies)
+  }
+
   return (
     <div className="App">
       <div className='search_nav'>
@@ -22,16 +33,16 @@ function App() {
         </div>
 
         <div>
-          <form>
-            <input />
+          <form onSubmit={handleSearch}>
+            <input onChange={(e) => setTerm(e.target.value)} />
             <button>Search</button>
           </form>
         </div>
       </div>
 
       <div className='movies'>
-        {movies.map((movie) => (
-          <MovieCard  {...movie}/>
+        {movies.map((movie, i) => (
+          <MovieCard  {...movie} key={i}/>
         ))}
       </div>
     </div>
--- src/MovieCard.js
+++ src/MovieCard.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import './MovieCard.css'
 
 const MovieCard = (props) => {
     const API_IMG = "https://image.tmdb.org/t/p/w500/"
     return (
         <div className='card'>
             <div className='poster'>
-                <img src={API_IMG + props.poster_path} />
+                <img src={API_IMG + props.poster_path} alt={props.title}/>
             </div>
 
             <div className='info'>
