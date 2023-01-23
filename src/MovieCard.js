f9d7f9be47659f409250be0adb78d9ad48655ac1
Rukshan Dias <rukshandias002@gmail.com>
2023-01-23 21:36:54 +0530

    no moview img found

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './MovieCard.css'
+import noImgFound_img from './no_img_found.png'
 
 const MovieCard = (props) => {
     const API_IMG = "https://image.tmdb.org/t/p/w500/"
+
     return (
         <div className='card'>
             <div className='poster'>
-                <img src={API_IMG + props.poster_path} alt={props.title}/>
+                <img src={props.poster_path ? API_IMG + props.poster_path : noImgFound_img} alt={props.title}/>
             </div>
 
             <div className='info'>
