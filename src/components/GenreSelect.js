5f0a8e2836a6380ae7a6f14f93ca98e65e91c330
Rukshan Dias <rukshandias002@gmail.com>
2023-01-30 14:49:28 +0530

    toggle genre select & styling

--- src/components/GenreSelect.js
+++ src/components/GenreSelect.js
@@ -1,33 +1,46 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
+
 
 const menuItems = [
     {
-        title : 'Adventure',
-        id : 12
+        title: 'Adventure',
+        id: 12
     },
     {
-        title : 'Adventure',
-        id : 12
+        title: 'Adventure',
+        id: 12
     },
     {
-        title : 'Adventure',
-        id : 12
+        title: 'Adventure',
+        id: 12
     },
     {
-        title : 'Adventure',
-        id : 12
+        title: 'Adventure',
+        id: 12
     },
 ]
 
-function GenreSelect() {
+const toggleGenreSelect = () => {
+
+}
+
+const GenreSelect = (props) => {
+    const [genre, setGenre] = useState('Select')
+    const [isGenreOpen, setIsGenreOpen] = useState(false)
+
     return (
-        <div className='genre'>
-            <ul>
-                {menuItems.map (item => (
-                    <li> {item.title} </li>
-                ))}
-            </ul>
-        </div>
+        <React.Fragment>
+            <div className='genre' onClick={() => setIsGenreOpen(!isGenreOpen)}>{genre}</div>
+            {isGenreOpen &&
+                <div className='GenreSelect'>
+                    <ul>
+                        {menuItems.map(item => (
+                            <li> {item.title} </li>
+                        ))}
+                    </ul>
+                </div>}
+        </React.Fragment>
     )
 }
 
